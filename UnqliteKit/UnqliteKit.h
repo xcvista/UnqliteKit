b6b4a4860f4c209756c2cc8b7b6d961168b9bb2e
Maxthon Chan <xcvista@me.com>
2015-04-02 18:43:37 +0800

    Initial Commit

---
+++ UnqliteKit/UnqliteKit.h
@@ -0,0 +1,19 @@
+//
+//  UnqliteKit.h
+//  UnqliteKit
+//
+//  Created by Maxthon Chan on 4/2/15.
+//  Copyright (c) 2015 DreamCity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UnqliteKit.
+FOUNDATION_EXPORT double UnqliteKitVersionNumber;
+
+//! Project version string for UnqliteKit.
+FOUNDATION_EXPORT const unsigned char UnqliteKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnqliteKit/PublicHeader.h>
+
+
